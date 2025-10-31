5232d0190e0cf35c79c72eb545d9a6d07642f8c5
um4ss <sliziard@icloud.com>
2025-10-31 14:14:54 +0100

    feat(utils): implement Input class for input management

--- include/utils/Input.hpp
+++ include/utils/Input.hpp
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 21:34:40 by sliziard          #+#    #+#             */
-/*   Updated: 2025/10/31 12:28:29 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/10/31 14:10:57 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <stdint.h>
 # include <string>
 
+# include "./PegException.hpp"
+
 # define PRINT_CALLER
 
 class Input {
@@ -25,28 +27,43 @@ private:
 	std::string	_data;
 	size_t		_pos;
 
+	static const char	*WHITESPACES;
+
 public:
+	class InputUnexpectedEof : public PegException {
+	public:
+		InputUnexpectedEof() : PegException("Input: unexpected EOF")
+		{}
+		InputUnexpectedEof(const std::string &msg) : PegException("Input: " + msg)
+		{}
+	};
+
 	Input();
 	Input(const std::string &text);
 	Input(const char *file_path);
 	Input(const Input& other);
 	~Input();
 
-	Input& operator=(const Input& other);
+	Input&	operator=(const Input& other);
+	char	operator*() const;
+	Input	&operator++();
 
-	bool		match(const std::string &literal);
-	char		get(void);
-	char		peek(void) const;
 	bool		eof(void) const;
+
+	char		peek(void) const;
+	char		get(void);								/* in case of eof : throw an expection */
+	bool		match(const std::string &literal);		/* in case of eof: return '\0' */
 
 	size_t		pos(void) const;
 	void		setPos(size_t newPos);
 
-	std::string	substr(size_t start, size_t end) const;
-
 	size_t		remaining(void) const;
 	size_t		line(void) const;
 	size_t		column(void) const;
+
+	std::string	substr(size_t start, size_t end) const;
+	std::string	context(size_t pos, size_t radius) const;
+	void		skipWhitespace(void);					/* Only skip ASCII whitespace (no support of LANG_C) */
 };
 
 #endif
---
+++ src/lib/utils/Input.cpp
@@ -0,0 +1,190 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Input.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 12:46:56 by sliziard          #+#    #+#             */
+/*   Updated: 2025/10/31 15:03:17 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstddef>
+#include <cstring>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "utils/Input.hpp"
+#include "utils/PegException.hpp"
+
+const char * Input::WHITESPACES = " \n\t\r";
+
+// *Constructors
+Input::Input(): _data(), _pos(0)
+{}
+
+Input::Input(const std::string &text): _data(text), _pos(0)
+{}
+
+// ? Good practice to pass binary flag for open a file for portability
+Input::Input(const char *file_path): _data(), _pos(0)
+{
+	std::ifstream	file(file_path, std::ios::in | std::ios::binary);
+
+	if (!file.is_open())
+		throw PegException(
+			"Input: failed to open file '" + std::string(file_path) + "'"
+		);
+	
+	std::ostringstream buf;
+	buf << file.rdbuf();
+	if (file.bad())
+		throw PegException(
+			"Input: error while reading file '" + std::string(file_path) + "'"
+		);
+
+	_data = buf.str();
+}
+
+Input::Input(const Input& other): _data(other._data), _pos(other._pos)
+{}
+
+// *Destructor
+Input::~Input()
+{}
+
+// *Operators
+
+Input& Input::operator=(const Input& other) {
+	if (this != &other)
+	{
+		_pos = other._pos;
+		_data = other._data;
+	}
+	return *this;
+}
+
+char	Input::operator*() const
+{
+	return peek();
+}
+
+Input	&Input::operator++()
+{
+	++_pos;
+	return *this;
+}
+
+//* Methods
+
+bool		Input::eof(void) const
+{
+	return (_pos > _data.size());
+}
+
+bool		Input::match(const std::string &literal)
+{
+	size_t	len = literal.size();
+
+	if (eof() || _pos + len > _data.size() ||
+		std::strncmp(_data.c_str() + _pos, literal.c_str(), len))
+		return false;
+
+	_pos += len;
+	return true;
+}
+
+char		Input::get(void)
+{
+	if (eof())
+		throw InputUnexpectedEof();
+	return _data[_pos++];
+}
+
+char		Input::peek(void) const
+{
+	if (eof())
+		return '\0';
+	return _data[_pos];
+}
+
+size_t		Input::pos(void) const
+{
+	return _pos;
+}
+
+void		Input::setPos(size_t newPos)
+{
+	_pos = newPos;
+}
+
+size_t		Input::remaining(void) const
+{
+	size_t	size = _data.size();
+	if (_pos >= size) return (0);
+	return size - _pos;
+}
+
+size_t		Input::line(void) const
+{
+	size_t	i = 0;
+	size_t	ln_count = 1;
+
+	while (i < _pos)
+	{
+		if (_data[i] == '\n')
+			++ln_count;
+		++i;
+	}
+	return (ln_count);
+}
+
+size_t		Input::column(void) const
+{
+	size_t	lst_ln;
+	
+	lst_ln = _data.rfind('\n', _pos == 0 ? 0 : _pos - 1);
+	if (lst_ln == std::string::npos)
+		return _pos + 1;
+	return _pos - lst_ln;
+}
+
+std::string	Input::substr(size_t start, size_t end) const
+{
+	return _data.substr(start, end - start);
+}
+
+std::string	Input::context(size_t pos, size_t radius) const
+{
+	size_t	start;
+	size_t	end;
+	size_t	size = _data.size();
+
+	if (pos > radius)
+		start = pos - radius;
+	else
+		start = 0;
+	if (pos + radius < size)
+		end = pos + radius;
+	else
+		end = size;
+	return substr(start, end);
+}
+
+void		Input::skipWhitespace(void)
+{
+	uint8_t	c;
+
+	while (!eof())
+	{
+		c = _data[_pos];
+		if (std::strchr(WHITESPACES, c))
+			_pos++;
+		else
+			break ;
+	}
+}
